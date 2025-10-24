b594c5a24af2ac47047a1684b3fa810de1c78518
Guilherme Gomes Botelho <95437442+guimagedon@users.noreply.github.com>
2025-10-24 11:40:01 -0300

    Update main.py
    Adição do ordenador do vetor de processos

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+# Classe Processo
 class Processo:
     def __init__(self, time_crt, time_exec, prior, pos):
-        self.time_crt = time_crt
-        self.time_exec = time_exec
-        self.prior = prior
-        self.wait_time = 0
-        self.state = "Ready"
-        self.endtime = -1
-        self.name = "P" + str(pos)
+        self.time_crt = time_crt # Tempo de criação do processo
+        self.time_exec = time_exec # Tempo de execução restante
+        self.prior = prior # Prioridade do processo
+        self.wait_time = 0 # Tempo de espera do processo
+        self.state = "Ready" # Estado do processo
+        self.endtime = -1 # Tempo em que o processo acabou
+        self.name = "P" + str(pos) # Nome do processo
 
 # Lendo o arquivo de configuração e obtendo...
 arquivo_config = "config.txt"
@@ -20,12 +21,16 @@ except FileNotFoundError:
 except Exception as e:
     print(f"Ocorreu o seguinte erro: {e}")    
 
-Processos = []
+Processos = [] # Esse vetor armazenará todos os processos inseridos pelo usuário
 i = 1
 while True:
-    entrada = input("").split()
+    entrada = input("").split() # Obtendo as entradas do usuário
     if not entrada:
-        break
+        break # Se o usuário mandar uma linha vazia, ele para de ler as entradas
     Processos.append(Processo(entrada[0], entrada[1], entrada[2], i))
     print(Processos[i - 1].name)
     i += 1
+
+Processos.sort(key=lambda p: p.time_crt)
+for j in Processos:
+    print(j.time_crt)
