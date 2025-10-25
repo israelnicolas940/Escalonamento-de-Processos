e91b12323927f4245d9ec702b609f7b881a6b6b6
Guilherme Gomes Botelho <95437442+guimagedon@users.noreply.github.com>
2025-10-25 19:45:55 -0300

    Update main.py
    Versão final da main

--- main.py
+++ main.py
@@ -1,85 +1,13 @@
-# Classe Processo
-class Processo:
-    def __init__(self, time_crt, time_exec, prior, pos):
-        self.time_crt = time_crt # Tempo de criação do processo
-        self.time_exec = time_exec # Tempo de execução restante
-        self.prior = prior # Prioridade do processo
-        self.wait_time = 0 # Tempo de espera do processo
-        self.time_rmng= time_exec # Tempo restante para terminar a execução
-        self.end_time = -1 # Tempo em que o processo acabou
-        self.pid = "P" + str(pos) # Nome do processo
-
-# Função que computa a espera de todos os processos disponíveis, mas que não executaram
-def Waiting(Disponiveis, Terminados, pid):
-    for p in Disponiveis:
-        if(p.pid != pid and p not in Terminados):
-            p.wait_time += 1
-
-def Scheduler_FCFS(Processos, Disponiveis, Terminados):
-    time = 0 # Tempo inicial
-    Timeline = [] # Linha do tempo especificando qual processo executou em dado momento
-    while(len(Processos) > len(Terminados)): # Enquanto existirem processos inacabados...
-        for p in Processos:
-            if(p.time_crt <= time and p not in Disponiveis and p not in Terminados):
-                Disponiveis.append(p) # Se o a data de criação for menor que o tempo atual, o 
-                                      # O processo torna-se disponível
-
-        if(len(Disponiveis) == 0):
-            Timeline.append("-") # Para o caso de ainda não terem sido criados processos
-            time += 1
-            continue
-        
-        proc_atual = Disponiveis.pop(0) # Escolhe-se o primeiro processo que chegou
-        while(proc_atual.time_rmng > 0):
-            for p in Processos: # Sempre checa se novos processos estão disponíveis
-                if(p.time_crt <= time and p not in Disponiveis and p not in Terminados):
-                    Disponiveis.append(p)
-
-            proc_atual.time_rmng -= 1 # desconta-se 1 do tempo restante para encerrar o processo escolhido
-            Waiting(Disponiveis, Terminados, proc_atual.pid) # Todos os processos disponíveis que não estão executando, esperam
-            Timeline.append(proc_atual.pid) # Adiciona-se à linha do tempo o processo que executou
-            time += 1
-            if(proc_atual.time_rmng == 0): # Se o processo acabou...
-                proc_atual.end_time = time # Obtemos seu tempo de finalização e...
-                Terminados.append(proc_atual) # Adicionamos ele à lista de terminados
-    return Timeline
-
-def Scheduler_SJF(Processos, Disponiveis, Terminados):
-    time = 0 # Tempo inicial
-    Timeline = [] # Linha do tempo especificando qual processo executou em dado momento
-    while(len(Processos) > len(Terminados)): # Enquanto existirem processos inacabados...
-        for p in Processos:
-            if(p.time_crt <= time and p not in Disponiveis and p not in Terminados):
-                Disponiveis.append(p) # Se o a data de criação for menor que o tempo atual, o 
-                                      # O processo torna-se disponível
-
-        if(len(Disponiveis) == 0):
-            Timeline.append("-") # Para o caso de ainda não terem sido criados processos
-            time += 1
-            continue
-        
-        # Escolhe o processo de menor duração e o retira da lista de processos disponíveis
-        proc_atual = Disponiveis.pop(Disponiveis.index(min(Disponiveis, key=lambda p: p.time_exec)))
-        while(proc_atual.time_rmng > 0):
-            for p in Processos: # Sempre checa se novos processos estão disponíveis
-                if(p.time_crt <= time and p not in Disponiveis and p not in Terminados):
-                    Disponiveis.append(p)
-
-            proc_atual.time_rmng -= 1 # desconta-se 1 do tempo restante para encerrar o processo escolhido
-            Waiting(Disponiveis, Terminados, proc_atual.pid) # Todos os processos disponíveis que não estão executando, esperam
-            Timeline.append(proc_atual.pid) # Adiciona-se à linha do tempo o processo que executou
-            time += 1
-            if(proc_atual.time_rmng == 0): # Se o processo acabou...
-                proc_atual.end_time = time # Obtemos seu tempo de finalização e...
-                Terminados.append(proc_atual) # Adicionamos ele à lista de terminados
-    return Timeline
+from processo import Processo
+from utilidades import Calc_AvgWaitTime, Calc_AvgWaitTurn
+from schedulers import Scheduler_FCFS, Scheduler_SJF, Scheduler_SRTF, Scheduler_PrioSp, Scheduler_PrioCp, Scheduler_RRSp, Scheduler_RRCp
 
 # Lendo o arquivo de configuração e obtendo...
 arquivo_config = "config.txt"
 try:
     with open(arquivo_config, "r", encoding="utf-8") as configuracao:
-        tam_quantum = configuracao.readline().strip() #... o tamanho do quantum e...
-        aging = configuracao.readline().strip() #... o grau de envelhecimento
+        tam_quantum = configuracao.readline().strip().split(':')[1] #... o tamanho do quantum e...
+        aging = configuracao.readline().strip().strip().split(':')[1] #... o grau de envelhecimento
         print(tam_quantum, aging) # Teste (Apagar depois)
 except FileNotFoundError:
     print(f"Erro: o arquivo", arquivo_config, "nao foi encontrado")   
@@ -99,7 +27,9 @@ while True:
 Processos.sort(key=lambda p: p.time_crt)
 Disponiveis = []
 Terminados = []
+cntxt_chngs = 0
 
-print(Scheduler_SJF(Processos, Disponiveis, Terminados))
-for p in Terminados:
+print(Scheduler_RRCp(Processos, Disponiveis, Terminados, cntxt_chngs, tam_quantum, aging))
+for p in Processos:
     print(p.pid, p.wait_time, p.end_time)
+print(Calc_AvgWaitTime(Processos), Calc_AvgWaitTurn(Processos))
